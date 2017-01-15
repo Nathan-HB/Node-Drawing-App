5086e1cc01e1e71798211d0c8a17e3871bff597f
Nathan-HB <nhansonb@gmail.com>
2017-01-15 15:59:37 +0000

    web server working and reading html

--- The Business/web/web.js
+++ The Business/web/web.js
@@ -1,2 +1,13 @@
-var http = require("http");
+var http = require('http');
+var fs = require('fs');
+var port = 3000;
 
+var server =  http.createServer(function(req, res){
+    fs.readFile('D:/Documents/development/workspace/nodeDrawApp/Node-Drawing-App/The Business/HTMLs/index.html',function (err, data){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+    });
+});
+
+server.listen(port);
