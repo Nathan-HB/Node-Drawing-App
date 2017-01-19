65767fd2c4f0b3ae4c6be0ca81eade8e27feec57
Nathan-HB <nhansonb@gmail.com>
2017-01-19 23:36:03 +0000

    added style and html

--- The Business/web/web.js
+++ The Business/web/web.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var port = 3000;
 
 var server =  http.createServer(function(req, res){
-    fs.readFile('D:/Documents/development/workspace/nodeDrawApp/Node-Drawing-App/The Business/HTMLs/index.html',function (err, data){
+    fs.readFile('../HTMLs/index.html',function (err, data){
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(data);
         res.end();
